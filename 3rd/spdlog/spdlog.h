8fc662bfd1a6071819288abad74eaed76fdb21a3
caobin <mcaobin@qq.com>
2025-04-09 17:13:33 +0800

    引入spdlog

---
+++ 3rd/spdlog/spdlog.h
@@ -0,0 +1,14 @@
+﻿
+#ifndef SPDLOG_H_H_
+#define SPDLOG_H_H_
+
+
+#define SPDLOG_HEADER_ONLY
+
+#include "spdlog-1.13.0/include/spdlog/spdlog.h"
+
+
+
+
+
+#endif // SPDLOG_H_H_
---
+++ 3rd/spdlog/spdlog_sink.h
@@ -0,0 +1,14 @@
+﻿
+#ifndef SPDLOG_SINK_H_
+#define SPDLOG_SINK_H_
+
+
+#define SPDLOG_HEADER_ONLY
+
+#include "spdlog-1.13.0/include/spdlog/sinks/rotating_file_sink.h"
+
+
+
+
+
+#endif // SPDLOG_SINK_H_
