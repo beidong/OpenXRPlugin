5f23c4520dc3135d4a815d7ba2ff8fe4562bb73c
caobin <mcaobin@qq.com>
2025-04-09 14:03:40 +0800

    camke项目组织暂存

---
+++ 3rd/nlohmann_json/json.h
@@ -0,0 +1,27 @@
+﻿
+#ifndef JSON_nlohmann_H
+#define JSON_nlohmann_H
+
+/******************************************************************
+  Rainfe 瑞风协同
+  Copyright (c) 2023 - All Rights Reserved.
+  ------------------------------------------------------------------
+  当前版本: 1.0
+  作    者:
+  时    间: 2023-03-08 16:33:53
+  注    释: ...   
+   
+  https://github.com/nlohmann/json
+
+  head only的json库，不用链接，只要包含一个头文件即可
+  
+******************************************************************/
+
+
+#include "single_include/nlohmann/json.hpp"
+
+
+
+
+
+#endif // JSON_nlohmann_H
